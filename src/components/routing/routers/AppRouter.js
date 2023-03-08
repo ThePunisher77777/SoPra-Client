d4fcf9fa665e20156b94336ddffda07bfbd66f8b
Mika Schoch <98819916+ThePunisher77777@users.noreply.github.com>
2023-03-08 17:12:54 +0100

    Remove unused import

--- src/components/routing/routers/AppRouter.js
+++ src/components/routing/routers/AppRouter.js
@@ -1,7 +1,6 @@
 import {BrowserRouter, Redirect, Route, Switch} from "react-router-dom";
 import {GameGuard} from "components/routing/routeProtectors/GameGuard";
 import GameRouter from "components/routing/routers/GameRouter";
-import {LoginGuard} from "components/routing/routeProtectors/LoginGuard";
 import Login from "components/views/Login";
 import Registration from "components/views/Registration";
 import UsersOverview from "../../views/UsersOverview";
